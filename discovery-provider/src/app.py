0abd7083db4088d2683fd5c15a732490759d2782
jowlee <joeylee0925@gmail.com>
2021-08-05 17:00:35 +0000

    Add reward manager transfer instruction indexing

--- discovery-provider/src/app.py
+++ discovery-provider/src/app.py
@@ -364,6 +364,7 @@ def configure_celery(flask_app, celery, test_config=None):
             "src.tasks.index_user_bank",
             "src.tasks.index_eth",
             "src.tasks.index_oracles",
+            "src.tasks.index_rewards_manager",
         ],
         beat_schedule={
             "update_discovery_provider": {
@@ -449,6 +450,10 @@ def configure_celery(flask_app, celery, test_config=None):
             "index_oracles": {
                 "task": "index_oracles",
                 "schedule": timedelta(minutes=5),
+            },
+            "index_rewards_manager": {
+                "task": "index_rewards_manager",
+                "schedule": timedelta(minutes=1),
             },
         },
         task_serializer="json",
